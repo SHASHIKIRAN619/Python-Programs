559bfbbeb0b5df1f46d3183813cede6c2083ac0a
shashi619 <98157823+SHASHIKIRAN619@users.noreply.github.com>
2024-11-06 14:40:37 +0530

    Add files via upload

---
+++ addtwonumbers.py
@@ -0,0 +1,7 @@
+#Write a python program to add two numbers:-
+print("Enter the first number: ");
+num1=int(input());
+print("Enter the Second number ");
+num2=int(input());
+sum=num1+num2;
+print("The sum of two numbers is:",sum);
