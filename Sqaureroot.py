9557880b46a4291d19d73d35ec863b8938f89088
shashi619 <98157823+SHASHIKIRAN619@users.noreply.github.com>
2024-11-06 14:57:50 +0530

    Add files via upload

---
+++ Sqaureroot.py
@@ -0,0 +1,16 @@
+#Write a progarm to find a square of a number:-
+import math
+
+print("Enter the number to find the squareroot: ");
+num=int(input())
+res=int(math.sqrt(num));
+print("The result is ",res);
+
+print("=======================================================================");
+
+#Write a number to find the square of a number:-
+
+print("Enter the number to find the square of a number: ");
+num1=int(input());
+res1=num1*num1;
+print("The Square of a number is ",res1);
