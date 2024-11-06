4761bdc07d5bc4601fa69050fa2d03021a23b39c
shashi619 <98157823+SHASHIKIRAN619@users.noreply.github.com>
2024-11-06 15:06:10 +0530

    Add files via upload

---
+++ Areaoftriangle.py
@@ -0,0 +1,8 @@
+#Write a program to find the Area of a Triangel:-
+
+print("Enter the height: ");
+height=float(input());
+print("Enter the base: ");
+base=float(input());
+Area_of_triangle = 1/2*height*base;
+print("The result is :",Area_of_triangle);
