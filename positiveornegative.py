bb10b5d4a14bfb8c9b870eb3554915e46ca41280
shashi619 <98157823+SHASHIKIRAN619@users.noreply.github.com>
2024-11-13 13:24:54 +0530

    Add files via upload

---
+++ positiveornegative.py
@@ -0,0 +1,16 @@
+#Write a program to check number is positive or negative?
+print("Enter the number: ");
+num=int(input());
+if(num>0):
+    print(f"The number {num} is positive");
+elif(num==0):
+    print(f"The number {num} is Zero")
+else:
+    print(f"The number {num} is negative");
+
+
+    
+
+  
+
+        
