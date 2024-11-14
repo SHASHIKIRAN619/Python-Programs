206d9a98a613fef1f1e8814542a8c28307a7db39
shashi619 <98157823+SHASHIKIRAN619@users.noreply.github.com>
2024-11-14 15:00:14 +0530

    Add files via upload

---
+++ randomnumber.py
@@ -0,0 +1,10 @@
+import random;
+#Write a program to generate random numbers:-
+
+class Random:
+    def randomnumber(self):
+        num=random.randint(1,10);
+        print(num);
+c1=Random();
+c1.randomnumber();
+        
